d815bf12246080a9ed9b07765ace43e4bc339611
Agent-grey <markkibuthu@gmail.com>
2024-08-10 22:47:53 +0300

    complete lab

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return (
-    <>
-      <header>
-        
-      </header>
-    </>
-  );
-};
+    return (
+        <>
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </>
+    );
+}
 
 export default App;
